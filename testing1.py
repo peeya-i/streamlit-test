4fbeabe6d11cc8dc9f64562ed5d59bf493f818e4
peeya-i <35793565+peeya-i@users.noreply.github.com>
2025-08-07 19:27:58 +0000

    Update testing1.py

--- testing1.py
+++ testing1.py
@@ -2,7 +2,13 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-st.table(dataframe)
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
